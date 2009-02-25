8d7976b64eb29d82a8ec1e03f6de37bbaa6c3f68
Tobias Gesellchen <tobias@gesellix.de>
2009-02-25 23:20:08 +0000

    [#15249] B1/C2: use configuration parameters todo: rename goto.php on configuration change?
    git-svn-id: http://joomlacode.org/svn/gesellix/shortlink@14 53e8e788-632f-0410-ae21-c9a27eba0831

--- com_shortlink/joomla_1_5/site/goto.php
+++ com_shortlink/joomla_1_5/site/goto.php
@@ -1,7 +1,26 @@
 <?php
 
+// Set flag that this is a parent file
+define( '_JEXEC', 1 );
+
+define('JPATH_BASE', dirname(__FILE__) );
+
+define( 'DS', DIRECTORY_SEPARATOR );
+
+require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
+require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+
+$mainframe =& JFactory::getApplication('site');
+
+$params = &JComponentHelper::getParams( 'com_shortlink' );
+
 // default location
-$link = "index.php?option=com_shortlink&phrase=".$_GET['link'];
+$link = "index.php";
+
+$phrase = $_GET[$params->get('paramname', 'link')];
+if ($phrase) {
+	$link = "index.php?option=com_shortlink&phrase=".$phrase;
+}
 
 // redirect to new location
 header("Location: $link");
