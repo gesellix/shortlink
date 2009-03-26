0fc719c7a9af3fce622b7cf00d51e76faa6e5669
Tobias Gesellchen <tobias@gesellix.de>
2009-03-26 23:28:17 +0000

    [#15269] Validate Preference changes
    git-svn-id: http://joomlacode.org/svn/gesellix/shortlink@31 53e8e788-632f-0410-ae21-c9a27eba0831

--- com_shortlink/joomla_1_5/site/goto.php
+++ com_shortlink/joomla_1_5/site/goto.php
@@ -8,32 +8,79 @@ define( 'DS', DIRECTORY_SEPARATOR );
 /* DON'T EDIT THE FOLLOWING LINE! IT CAN BE EDITED IN THE COMPONENT CONFIGURATION. */
 define('JPATH_BASE', dirname(__FILE__) );
 
-require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
-require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+// get component parameters
+$params = getParams();
 
-$mainframe =& JFactory::getApplication('site');
-
-$params = &JComponentHelper::getParams( 'com_shortlink' );
-
-// default location
-$link = "index.php";
-
-foreach(explode(",", $params->get('paramname', 'link')) as $linkname) {
-  $phrase = $_GET[$linkname];
-  if ($phrase)
-  {
-  	$url_path = $params->get('url_path', '');
-  	if ( !empty( $url_path ) && substr( $url_path, -1 ) != DIRECTORY_SEPARATOR )
-  	{
-		$url_path .= DIRECTORY_SEPARATOR;
-	}
-
-    $link = $url_path."index.php?option=com_shortlink&phrase=".$phrase;
-    break;
-  }
-} // foreach
+// generate target link. Defaults to 'index.php' if no valid link/phrase is found
+$link = getLink($params);
 
 // redirect to new location
-header("Location: $link");
+header("Location: ".$link);
 
+
+////////////////////////////////////////////////////
+// internal functions
+
+
+
+function getParams()
+{
+	require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
+	require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+
+	$mainframe =& JFactory::getApplication('site');
+
+	$params = &JComponentHelper::getParams( 'com_shortlink' );
+
+	return $params;
+}
+
+function getLink($params)
+{
+	// default location
+	$link = "index.php";
+
+	$linknames = explode(",", $params->get('paramname', 'link'));
+	foreach($linknames as $linkname) {
+	  $phrase = $_GET[$linkname];
+	  if ($phrase)
+	  {
+	  	$url_path = getUrlPath();
+	
+	    $link = $url_path."index.php?option=com_shortlink&phrase=".$phrase;
+	    break;
+	  }
+	} // foreach
+	
+	return $link;
+}
+
+function getUrlPath()
+{
+  	$url_path = '';
+
+  	$this_dir = dirname(__FILE__);
+  	if ($this_dir != JPATH_BASE)
+  	{
+		if (strlen($this_dir) > strlen(JPATH_BASE))
+  		{
+  			$url_path = substr($this_dir, strlen(JPATH_BASE) + 1);
+  		}
+  		else if (strlen($this_dir) < strlen(JPATH_BASE))
+  		{
+  			$url_path = substr(JPATH_BASE, strlen($this_dir) + 1);
+    	}
+		else
+		{
+			// Houston, we've got a problem...
+		}
+  	}
+
+  	if ( !empty( $url_path ) && substr( $url_path, -1 ) != '/' )
+  	{
+		$url_path .= '/';
+	}
+  	
+  	return $url_path;
+}
 ?>
